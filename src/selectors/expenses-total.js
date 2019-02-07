c9eae0fc80fd3326b7c7cc79e96a22a40617cf2a
Michal Szpakowski <michal.szpakowski@netguru.co>
2019-02-07 22:15:04 +0100

    Add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,7 @@
+const getExpensesTotal = (expenses) => {
+  return expenses
+    .map((expense) => expense.amount)
+    .reduce((sum, amount) => sum + amount, 0);
+};
+
+export default getExpensesTotal;
