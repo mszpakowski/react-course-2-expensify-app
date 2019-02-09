17558e239de881744599674f4ff83649686a32df
Michal Szpakowski <michal.szpakowski@netguru.co>
2019-02-09 17:36:28 +0100

    Setup test database env

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
-import expenses from '../fixtures/expenses';
 
 test('should correctly render ExpensesSummary with 1 expense', () => {
   const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
